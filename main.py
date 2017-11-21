d096e290e48cd80c787d85661df1047461b658d7
David Greiner <mail@david-greiner.me>
2017-11-21 14:45:09 -0800

    startpoint for all the work

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import numpy as np
+
+datafile = "data.csv"
+
+if __name__ == "__main__":
+    data = pd.read_csv(datafile)
+
+    algorithm = int(input("Select what you want to do with the dataset > "))
+
+    if algorithm == 0:
+        #insert your algorithm here and pass data as argument
+    if algorithm == 1:
+        #insert your algorithm here and pass data as argument
+    
