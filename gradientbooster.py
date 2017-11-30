4470f9d6c35ea0c4a329311b71814d8397e5320e
Yogita Garud <garudyogitap@gmail.com>
2017-11-29 22:41:20 -0800

    Gradient boosting

--- gradientbooster.py
+++ gradientbooster.py
@@ -14,15 +14,18 @@ def gradient_booster(data, target):
 	data = data[:,2:]
 	data[:,-1] = 1
 	data = data.astype(float)
-	model= GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0)
-	model.fit(data, target)
 
-	train_error = model.score(data, target)
-	print("train_Error: ", train_error)
+	for i in range(150, 400, 50):
 
-	scores = cross_val_score(model, data, target, cv=10)
-    	#devArr.append(100-scores.mean())
-	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))	
+		model= GradientBoostingClassifier(n_estimators=i, learning_rate=0.5, max_depth=2, random_state=0)
+		model.fit(data, target)
+
+		train_error = model.score(data, target)
+		print("train_Error: ", train_error)
+
+		scores = cross_val_score(model, data, target, cv=5)
+	    	#devArr.append(100-scores.mean())
+		print("Accuracy: %0.2f (+/- %0.2f) for n_estimators %d " % (scores.mean(), scores.std() * 2, i))	
 
 	#Predict Output
 	#predicted= model.predict(x_test)
