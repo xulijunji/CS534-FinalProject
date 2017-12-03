ad9bea1af51a8f97c7092adc13a61f710a80a3d0
Yogita Garud <garudyogitap@gmail.com>
2017-12-03 10:07:22 -0800

    update

--- gradientbooster.py
+++ gradientbooster.py
@@ -21,6 +21,12 @@ def gradient_booster(data, target):
 	data[:,-1] = 1
 	data = data.astype(float)
 
+	#data = np.concatenate((data[:i], data[i+10:]), axis = 0)
+
+	data_red = np.delete(data, 25, 1)
+	data_red = np.delete(data_red, 22, 1)
+	data_red = np.delete(data_red, 21, 1)
+
 	#model= GradientBoostingClassifier(n_estimators=200, learning_rate=0.5, max_depth=1, random_state=0)
 	#rfecv = RFECV(estimator=model, step=1, cv=10,scoring='accuracy')   #5-fold cross-validation
 	#rfecv = rfecv.fit(data, target)
